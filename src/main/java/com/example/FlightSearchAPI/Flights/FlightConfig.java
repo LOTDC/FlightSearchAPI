f360aa57278224676aa262750becc2207b75811c
lotdc <kaan_bdk@outlook.com>
2024-02-10 20:41:11 +0300

    LocalDateTime editing

--- src/main/java/com/example/FlightSearchAPI/Flights/FlightConfig.java
+++ src/main/java/com/example/FlightSearchAPI/Flights/FlightConfig.java
@@ -18,17 +18,19 @@ public class FlightConfig {
                     "Dallas Fort Worth",
                     "flight1@gmail.com",
                     "Los Angeles",
-                    LocalDateTime.of(2019, 3, 28, 14, 33, 48, 490000000),
-                    LocalDateTime.of(2019, 3, 30, 17, 45, 48, 560000000),
+                    LocalDateTime.of(2019, 3, 28, 14, 33, 48, 49),
+                    LocalDateTime.of(2019, 3, 30, 17, 45, 48, 56),
                     55.12);
 
             Flight flight2 = new Flight(
                     "Toronto Pearson",
                     "flight2@gmail.com",
                     "Miami",
-                    LocalDateTime.of(2023, 3, 28, 14, 33, 48, 490000000),
-                    LocalDateTime.of(2023, 4, 5, 8, 25, 14, 560000000),
+                    LocalDateTime.of(2023, 3, 28, 14, 33, 48, 32),
+                    LocalDateTime.of(2023, 4, 5, 8, 25, 14, 17),
+                    LocalDateTime.of(2023, 11, 6, 7, 25, 55, 23),
                     113.45);
+
 
             repository.saveAll(
                     List.of(flight1,flight2)
